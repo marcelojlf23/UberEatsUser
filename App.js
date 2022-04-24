432f96d6fd6277ebac9912fe3011a7af9f2d7836
marcelojlf23 <marcelo.jlf23@gmail.com>
2022-04-24 15:53:37 -0300

    creating first structure to frist component
    creating first structure to frist component

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.restaurantContainer}>
+        <Image 
+          source={{ 
+            uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg"
+          }} 
+          style={styles.image}
+        />
+        <Text style={styles.title}>El Cabo Coffe Bar Tres Tres de May</Text>
+        <Text style={styles.subtitle}>$ $1.99 15-30 minutes</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +26,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  restaurantContainer:  {
+    width: '100%',
+    aspectRatio: 4/3,
+  }, 
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "500",
+    marginVertical: 5,
+  }, 
+  subtitle: {
+    color: 'gray',
+  }
 });
