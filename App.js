1760fdc2b49fbd8cf050a67d0b7f87ebf5eff728
marcelojlf23 <marcelo.jlf23@gmail.com>
2022-04-25 08:30:43 -0300

    implementing the FlatList
    implementing the FlatList to restaurant component

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 import RestaurantItem from './src/components/RestaurantItem';
 import restaurants from './assets/data/restaurants.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RestaurantItem restaurant={restaurants[0]}/>
-      <RestaurantItem restaurant={restaurants[1]}/>
-      <RestaurantItem restaurant={restaurants[2]}/>
+      <FlatList
+        data={restaurants} 
+        renderItem={({ item }) => <RestaurantItem restaurant={ item }/>}
+        showVerticalScroolIndicator={false}
+      />
       <StatusBar style="auto" />
     </View>
   );
