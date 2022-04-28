ec2f383619e6feb37d35193219dfab24f0b7ea5a
marcelojlf23 <marcelo.jlf23@gmail.com>
2022-04-27 22:43:41 -0300

    implementing DishDetailsScreen
    implementing DishDetailsScreen

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import DishDetailsScreen from './src/screens/DishDetailsScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import RestaurantDetailsScreen from './src/screens/RestaurantDetailsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RestaurantDetailsScreen />
+      <DishDetailsScreen />
+      {/* <RestaurantDetailsScreen /> */}
       {/* <HomeScreekn /> */}
 
       <StatusBar style="auto" hidden={true} />
