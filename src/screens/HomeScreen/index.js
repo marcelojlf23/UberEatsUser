d282ddb616b3c901f982320f009c025a2c9f78fb
marcelojlf23 <marcelo.jlf23@gmail.com>
2022-05-06 08:58:55 -0300

    linking to amplyfy database stoped in 1:48:47
    linking to amplyfy database stoped in 1:48:47

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,8 +1,21 @@
+import { useState, useEffect } from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
 import RestaurantItem from '../../components/RestaurantItem';
-import restaurants from '../../../assets/data/restaurants.json';
+import { DataStore } from 'aws-amplify';
+import { Restaurant } from '../../models';
 
 export default function HomeScreen() {
+  const [restaurants, setRestaurants] = useState([]); 
+
+  const fetchRestaurants = async () => {
+    const results = await DataStore.query(Restaurant);
+    setRestaurants(results); 
+  }
+
+  useEffect( () => {
+    fetchRestaurants();
+  }, []);
+  
   return (
     <View style={styles.page}>
       <FlatList
