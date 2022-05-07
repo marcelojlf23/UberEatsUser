c7e6b752381b27ffa67cd81de1aae234e4128b26
marcelojlf23 <marcelo.jlf23@gmail.com>
2022-05-07 15:46:59 -0300

    default image in header
    default image in header

--- src/screens/RestaurantDetailsScreen/Header.js
+++ src/screens/RestaurantDetailsScreen/Header.js
@@ -2,12 +2,12 @@ import { View, Text, Image,  } from 'react-native';
 import restaurants from '../../../assets/data/restaurants.json';
 const restaurant = restaurants[0];
 import styles from './styles';
-
+const DEFAULT_IMAGE = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg";
 
 const RestaurantHeader = ({restaurant}) =>{
     return (
         <View styles={styles.page}>
-            <Image source={{uri: restaurant.image}} style={styles.image}/>
+            <Image source={{uri: restaurant.image.startsWith('http') ? restaurant.image : DEFAULT_IMAGE}} style={styles.image}/>
 
             <View style={styles.containerRestaurantDetail}>
                 <Text style={styles.title}>{restaurant.name}</Text>
