e7ef0aa486154e14165972d4fa22f4e113b5954b
marcelojlf23 <marcelo.jlf23@gmail.com>
2022-05-17 22:39:52 -0300

    implementing update save/update and go back
    implementing update save/update and go back

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -4,24 +4,50 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Auth, DataStore } from "aws-amplify";
 import { User } from '../../models';
 import { useAuthContext } from "../../context/AuthContext";
+import { useNavigation } from "@react-navigation/native";
 
 const Profile = () => {
-  const [name, setName] = useState("");
-  const [address, setAddress] = useState("");
-  const [lat, setLat] = useState("0");
-  const [lng, setLng] = useState("0");
+  const { dbUser } = useAuthContext();
+
+  const [name, setName] = useState(dbUser?.name || "");
+  const [address, setAddress] = useState(dbUser?.address || "");
+  const [lat, setLat] = useState(dbUser?.lat + "" || "0");
+  const [lng, setLng] = useState(dbUser?.lng + "" || "0");
 
   const { sub, setDbUser } = useAuthContext();
 
-  const onSave = async () => {
-    try {
+  const navigation = useNavigation();
 
+
+  const onSave = async () => {
+    if (dbUser) {
+      await updateUser();
+    } else {
+      await createUser();
+    }
+    navigation.goBack();
+  }
+
+  const updateUser = async () => {
+    const user = await DataStore.save(
+      User.copyOf(dbUser, (updated) => {
+        updated.name = name;
+        updated.address = address;
+        updated.lat = parseFloat(lat);
+        updated.lng = parseFloat(lng);
+      })
+    );
+    setDbUser(user);
+  };
+
+  const createUser = async() => {
+    try {
       const user = await DataStore.save(new User({
-          name, 
-          address, 
-          lat: parseFloat(lat), 
-          lng: parseFloat(lng), 
-          sub
+        name, 
+        address, 
+        lat: parseFloat(lat), 
+        lng: parseFloat(lng), 
+        sub
         })
       );  
       console.log(user);
